72396deacc1e2f43e77a0ca635684035cfa80850
heymuse-lai <muse_lai@yahoo.com.tw>
2025-12-18 20:24:11 +0800

    公共區提示框toast 註冊頁面 完成

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
 // 配置axios基地址
 // axios.defaults.baseURL:'https://hmajax.itheima.net'
 axios.defaults.baseURL = 'https://hmajax.itheima.net'
+
+// 公共的提示框
+const showToast = (msg) =>{
+    // 透過 JavaScript 初始化 toast 提示框
+    // const toastElList = document.querySelectorAll('.toast')
+    // const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, option))
+
+    const myToast = document.querySelector('.my-toast')
+    const toastObj = new bootstrap.Toast(myToast)
+    toastObj.show()
+    document.querySelector('.toast-body').innerHTML=msg
+    
+}
---
+++ js/register.js
@@ -0,0 +1,22 @@
+//獲取btnDom 取得form資料 判斷資料是否為空 長度夠否
+document.querySelector('#btn-register').addEventListener('click', async e =>{
+    const data = serialize(document.querySelector('.register-form'), {hash:true , empty:true})
+    console.log(data)
+    if (!data.username) {
+        return showToast('名字不得為空')
+    }
+    if (!data.password) {
+        return showToast('密碼不得為空')
+    }
+    if (data.username.length < 8 || data.username.length > 30 ) {
+        return showToast('名字不符合規定')
+    }
+    if (data.username.length < 6 || data.username.length > 30) {
+        return showToast('密碼不符合規定')
+    }
+    const res = await axios.post('/register', data)
+    console.log(res);
+    
+    showToast(res.data.message)
+    location.href = './login.html'
+})
